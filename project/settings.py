e2e0761ef0e4b937e1f2784081996a679a9c361c
pk karki <prabin@abhiyantrik.com>
2020-09-14 08:36:54 +0545

    database chnage to postgresql

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,14 @@ WSGI_APPLICATION = "project.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "sample_project",
+        "USER": "prabin",
+        "PASSWORD": "prabin",
+        "HOST": "localhost",
+        "PORT": "",
+    }
 }
 
 
